0f7c6bf2c412a5ea021cf4964e683af0a7669e70
LucDeme <33514268+LucDeme@users.noreply.github.com>
2018-01-26 16:04:02 +0100

    Initial commit

---
+++ compte.php
@@ -0,0 +1,82 @@
+<link rel="stylesheet" href="./gg.css" >
+<link rel="stylesheet" href="./navbar.php" >
+<?php
+include("./navbar.php");
+include("./menu.php");
+
+try
+{
+	echo '<br />';
+    $bdd = new PDO('mysql:host=localhost;dbname=CDO;charset=utf8', 'root', '');
+    // echo'connexion OK <br />'; border="1" width="70%" align="center"
+}
+catch (Exception $e)
+{
+        die('Erreur : ' . $e->getMessage());
+}
+$reponse = $bdd->query('SELECT * FROM reservation, document' ); // IL FAUT FINIR LINNER JOIN POUR NOM PRENOM STAGIAIRE
+$nom = $bdd->query('SELECT * FROM utilisateur');
+$pret = $bdd->query('SELECT * FROM pret');
+?>
+<!--SELECT * FROM stage inner join entreprise on stage.id_entreprise=entreprise.id_entreprise'*/
+-->
+<?php $tab = $nom->fetch();?>
+   
+<h2>Bonjour <b><?php echo $tab['Nomutilisateur'];?></b> bienvenue sur votre espace perso </h2><p> demander ralonge de pret
+Listes de vos ouvrages emprunté:</p>
+<h1>reservation</h1>
+<table class="tblcontact" >
+<br>
+  <tr>
+    <th>ID reservation</th>
+    <th>Titre document</th>
+    <th>Auteur</th>
+    <th>Date reservation</th>
+  </tr> 
+
+  <?php //on affiche les ligne du tableau avec la boucle while
+  while ($tabs = $reponse->fetch())
+  {
+
+  ?>
+
+  <tr>
+    <td><?php echo $tabs['Idreservation'];?></td>
+    <td><?php echo $tabs['Titredocument'];?></td>
+    <td><?php echo $tabs['NomAuteurDocument'];?></td>
+    <td><?php echo $tabs['datereservation'];?></td>
+  </tr>
+<?php
+}
+?>
+</table>
+
+
+
+<h1>pret</h1>
+<table class="tblcontact" >
+  <br>
+    <tr>
+      <th>ID Pret</th>
+      <th>Date début de pret</th>
+      <th>Date fin de Pret</th>
+      <th>Nom pret</th>
+    </tr>     
+
+  <?php //on affiche les ligne du tableau avec la boucle while
+  while ($donnee = $pret->fetch())
+  {
+  ?>
+  
+  <tr>
+    <td><?php echo $donnee['IDpret'];?></td>
+    <td><?php echo $donnee['Datedebpret'];?></td>
+    <td><?php echo $donnee['Datefinpret'];?></td>
+    <td><?php echo $donnee['Nompret'];?></td>
+  </tr>
+  
+  <?php
+   }
+   $reponse->closeCursor(); //libère la connexion du serveur, permettant ainsi à d'autres requêtes SQL d'être exécutées
+  ?>
+</table>
---
+++ document.php
@@ -0,0 +1,94 @@
+<link rel="stylesheet" href="./gg.css" >
+<link rel="stylesheet" href="./navbar.php" >
+
+<?php
+include("./navbar.php");
+include("./menu.php");
+
+try
+{
+	echo '<br />';
+    $bdd = new PDO('mysql:host=localhost;dbname=CDO;charset=utf8', 'root', '');
+ // echo'connexion OK <br />';
+}
+catch (Exception $e)
+{
+        die('Erreur : ' . $e->getMessage());
+}
+$reponse = $bdd->query('SELECT * FROM document group by Titredocument');
+
+?>
+<br>
+    <table class="tblstage" >
+
+      <tr>
+        <th><b></b></th>
+        <th><b>resumé</b></th>
+          <th><b>Titre document</b></th>
+          <th>Auteur document</th>
+          <th>Theme document</th>
+          <th>Type document</th>
+          <th>Editeur document</th>
+          <th>Réserver</th>
+      </tr>
+    <?php //on affiche les ligne du tableau avec la boucle while
+while ($donnees = $reponse->fetch())
+{ ?>
+  <tr>
+      <th><?php echo "<input type='hidden' name='tab[".$donnees['IDdocument']."]' value='".$donnees['IDdocument']."'>" ?>
+      <th><?php echo $donnees['ResumeDocument'];?></th>
+      <th><?php echo $donnees['Titredocument'];?></th>
+      <th><?php echo $donnees['NomAuteurDocument'];?></th>
+      <th><?php echo $donnees['ThemeDocument'];?></th>
+      <th><?php echo $donnees['TypeDocument'];?></th>
+      <th><?php echo $donnees['EditeurDocument'];?></th>
+      <?php $reference =  $donnees['IDdocument']."¤".$donnees['Titredocument']."¤".$donnees['NomAuteurDocument']."¤".$donnees['ThemeDocument']."¤".$donnees['TypeDocument']
+      ."¤".$donnees['EditeurDocument']."¤".$donnees['ResumeDocument']; ?>
+      <th><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#ModalReserver_"
+       onclick="test('<?php echo $reference; ?>')">Réserver</button></th>
+  </tr>
+
+<?php }
+$reponse->closeCursor();
+?>
+</table>
+<!-- Modal -->
+<div class="modal fade" id="ModalReserver_" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h3 class="modal-title" id="exampleModalLabel">Réserver un document</h3>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body"> 
+        <span id="info"></span> <br>
+        <form action="action.php" method="post">
+
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <input type="submit" value="Sauvegarder" />
+      </form>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
+</body>
+<script type="text/javascript">
+ 
+function test(reference){
+ 
+var tabref = reference.split("¤");
+
+document.getElementById("info").innerHTML = "<b>Titre</b> : " + tabref[1] + " <br><b>Nom auteur</b> : " + tabref[2]  +" <br><b>TypeDocument </b>: " + tabref[4]+" <br><b>Resume Document </b>: " + tabref[6] ;
+
+  
+}
+
+</script>
